d4c6255e36e44a26f6daa7e60e409e0926c1db06
weeqeecombe50 <mynieagbona@hotmail.com>
2025-10-31 11:13:16 +0200

    Update log message for clarity and consistency

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 
-console.log('Willkommen bei Versionsverwaltung - Ihr Tool zur Verwaltung von Projektversionen!');
+console.log('Willkommen bei der Versionsverwaltung - Ihrem Tool zur Verwaltung von Projektversionen!');
 
 function startApp() {
     // Hier wird die Hauptlogik der Anwendung implementiert.
-    console.log('Die Anwendung wurde gestartet.');
+    console.log('Die Anwendung wurde erfolgreich gestartet.');
 }
 
 startApp();
