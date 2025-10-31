53ae8d17730f66c6f8c45aca4fb6a21592a0df0b
weeqeecombe50 <mynieagbona@hotmail.com>
2025-10-31 11:14:34 +0200

    Update welcome message for clarity

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-console.log('Welcome to the version control - your tool for managing project versions!');
+console.log('Welcome to the version control tool - your solution for managing project versions!');
 
 function startApp() {
     // Here the main logic of the application is implemented.
