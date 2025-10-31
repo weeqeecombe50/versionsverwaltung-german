eb00eaae2c739086cedb059586f0447afd93b482
weeqeecombe50 <mynieagbona@hotmail.com>
2025-10-31 11:15:29 +0200

    Refactor welcome message for clarity.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-console.log('Welcome to the Version Control Tool - Your Solution for Efficiently Managing Project Versions!');
+console.log('Welcome to the Version Control Tool - Your Solution for Efficient Management of Project Versions!');
 
 function startApp() {
     // Here the main logic of the application is implemented.
