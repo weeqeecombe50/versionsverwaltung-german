22fae7eb12ac6b1529ceb2c53c3e85f35ef98763
weeqeecombe50 <mynieagbona@hotmail.com>
2025-10-31 11:12:56 +0200

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+console.log('Willkommen bei Versionsverwaltung - Ihr Tool zur Verwaltung von Projektversionen!');
+
+function startApp() {
+    // Hier wird die Hauptlogik der Anwendung implementiert.
+    console.log('Die Anwendung wurde gestartet.');
+}
+
+startApp();
